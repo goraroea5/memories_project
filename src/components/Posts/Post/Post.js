bd283168a2aa5f65480c08b2dfc2a0f1ed82eaa9
Kang <kang.12@live.com>
2021-06-01 12:32:11 +0700

    conect server

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import useStyles from './styles';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import { ThumbUpAlt , Delete, MoreHoriz } from '@material-ui/icons';
+import moment from 'moment';
+import { useDispatch } from 'react-redux';
+
+
+import { deletePost, likePost } from '../../../actions/posts'
+
+const Post = ({post , setCurrentId}) => {
+  const dispatch = useDispatch()
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardMedia className={classes.media} image={post.selectFile} />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{color: 'white'}} size="small" onClick = {() => setCurrentId(post._id)}>
+          <MoreHoriz fontSize="default" />
+        </Button>
+      </div>
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map(tag => `#${tag} `)}
+        </Typography>
+      </div>
+      <Typography className={classes.title} variant="h5" gutterBottom>{ post.title }</Typography>
+      <CardContent>
+        <Typography variant="body2"  color="textSecondary" component="p">{post.message}</Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))} >
+          <ThumbUpAlt fontSize="small" />
+          &nbsp; Like   &nbsp; {post.likeCount}
+        </Button>
+        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))} >
+          <Delete fontSize="small" />
+          Delete
+        </Button>
+        
+      </CardActions>
+    </Card>
+  )
+}
+
+export default Post;
